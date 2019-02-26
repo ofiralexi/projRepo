e492c70e411ae8b7a84247e62bb15fca79b3044b
ofir alexi <ofiralexi@gmail.com>
2019-02-26 19:57:00 +0200

    supportedc clitn version

--- scripts/script.py
+++ scripts/script.py
@@ -2,38 +2,54 @@ import os
 import time
 import picamera
 import sys
-sys.path.append("/home/pi/Desktop/scripts/detecting")
-import detect
+import io
+import socket
+import struct
 
 
-def calc_direction(image_path):
-	return detect.find_balloon(image_path)
+def calc_direction():
+	pass
 
 
 def move_drone():
 	pass
 
 
-def take_picture(image_num):
+def take_picture(imagenum, connection):
+	if imagenum == 5: #send signal we're done
+		connection.write(struct.pack('<L', 0))
 	print("preparing to take picture...")
-	image_path = "images/image"+image_num+".jpg"
 	with picamera.PiCamera() as camera:
 		camera.resolution = (1280,720)
-		camera.capture(image_path)
-	print("calculating direction...")
-	print(calc_direction(image_path))
-	print("moving drone...")
-	move_drone()
-	print("==============================")
+		stream = io.BytesIO()
+		camera.capture(stream,'jpeg')
+		connection.write(struct.pack('<L', stream.tell()))
+		connection.flush()
+		# Rewind the stream and send the image data over the wire
+		stream.seek(0)
+		connection.write(stream.read())
+		print("sent to server")
+		# Reset the stream for the next capture
+		stream.seek(0)
+		stream.truncate()
+		print("===========================")
 
 
 # clear files in images
 dir_name = "./images"
 [os.remove(os.path.join(dir_name,f)) for f in os.listdir(dir_name)]
-#start the run
-i = 1
-print("==============================")
-while 1:
-	take_picture(str(i))
-	i = i + 1
-	# time.sleep(2)
+try:
+	# Connect a client socket to my_server:8000
+	print("trying to connect to server")
+	client_socket = socket.socket()
+	client_socket.connect(('132.68.47.189', 8000))
+	connection = client_socket.makefile('wb')
+	print("connected to server succesfuly")
+	#start the run
+	print("==============================")
+	for i in range(1,6):
+		take_picture(i, connection)
+		time.sleep(6)
+finally:
+	connection.close()
+	client_socket.close()
